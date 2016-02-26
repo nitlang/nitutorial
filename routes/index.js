b393571f85f8066ec96b16f3a0665b18e2a3720c
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-26 00:47:25 -0500

    routes: index displays a submit form and compile/exec the posted code
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var fs = require('fs');
+var child_process = require('child_process');
+
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+	var code = req.body.code;
+	if(!code) {
+		res.redirect("/");
+		return
+	}
+	var ucode = code.replace(/\r/g, "\n");
+	var tmpdir = "out/" + new Date().getTime();
+	child_process.execSync('mkdir -p ' + tmpdir);
+	fs.writeFileSync(tmpdir + '/prog.nit', ucode, 'utf-8');
+	child_process.exec('nitc --no-color ' + tmpdir + '/prog.nit -o ' + tmpdir + '/prog', {timeout: 5000},
+	  function(cerr, cstdout, cstderr) {
+		if(cerr) {
+			res.render('index', {code: code, err: cerr, stdout: cstdout, stderr: cstderr});
+			return
+		}
+		child_process.exec('timeout -k 3 3 firejail --profile=jail.profile --quiet --private=' + tmpdir + ' ./prog 2>&1 | grep -v "Reading" && rm -rf ', {timeout: 4000}, function(err, stdout, stderr) {
+			try {
+				child_process.execSync('rm -rf ' + tmpdir);
+			} catch(e) {
+				console.log(e);
+			}
+			res.render('index', {code: code, err: err, stdout: stdout, stderr: stderr});
+		});
+	});
 });
 
 module.exports = router;
